45619d79f7ebc5a7a4337ee36891cbadd4ef5176
sureshg <suresh.g@nuncsystems.com>
2022-02-08 20:27:15 +0530

    Code Commit 1

---
+++ android/app/src/main/kotlin/com/flutterlibraryoperations/flutter_library_operations/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterlibraryoperations.flutter_library_operations
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_library_nunc/lib/flutter_library_nunc.dart
@@ -0,0 +1,6 @@
+library flutter_library_nunc;
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
